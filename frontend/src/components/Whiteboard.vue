d36c75fbdc7fb56e607f54435c2b1e711ffa7759
kidchang93 <lck930721@gmail.com>
2024-06-21 16:28:46 +0900

    feat: websocket 사용중 생긴 오류 수정중

--- frontend/src/components/Whiteboard.vue
+++ frontend/src/components/Whiteboard.vue
@@ -1,5 +1,6 @@
 <template>
-    <canvas class="fabric" ref="canvas"></canvas>
+
+  <canvas class="fabric" ref="canvas"></canvas>
 
     <div id="designTool">
       <button type="button" class="btn btn-secondary" @click="buttonErase">모두 지우기</button>
@@ -8,15 +9,15 @@
       <br>
       <label for="drawing-line-width">Line width : </label>
       <span class="info">{{ lineWidth }}</span>
-      <input type="range" :value="lineWidth" min="0" max="100" id="drawing-line-width" ref="drawingLineWidthEl" @change="changeLineWidth"><br>
+      <input type="range" :value="lineWidth" min="0" max="100" id="drawing-line-width" ref="drawingLineWidthEl" @input="changeLineWidth"><br>
 
       <label for="drawing-color">그리기 색 : </label>
       <span class="info">{{ color }}</span>
-      <input type="color" :value="color" id="drawing-color" ref="drawingColorEl" @change="changeColor"><br>
+      <input type="color" :value="color" id="drawing-color" ref="drawingColorEl" @input="changeColor"><br>
 
       <label for="drawing-color">도형 색 : </label>
       <span class="info">{{ fillColor }}</span>
-      <input type="color" :value="fillColor" id="fill-color" ref="drawingFillColorEl" @change="changeFillColor"><br>
+      <input type="color" :value="fillColor" id="fill-color" ref="drawingFillColorEl" @input="changeFillColor"><br>
 
       <button type="button"
               class="btn btn-secondary"
@@ -75,7 +76,7 @@ export default {
 
       // fabric
       mode:'',
-      // brush:'',
+      brush:{},
       x:0,
       y:0,
       lineCap:'round',
@@ -124,39 +125,93 @@ export default {
         isDrawingMode: false,
 
       });
+      this.$refs.canvas.width = 1920;
+      this.$refs.canvas.height = 1080;
       // 브러시 초기 설정
       this.canvas.freeDrawingBrush.color = this.color;
-      this.canvas.freeDrawingBrush.width = this.lineWidth;      console.log("초기 설정 : ",this.canvas);
+      this.canvas.freeDrawingBrush.width = this.lineWidth;
+
+      console.log("초기 설정 : ",this.canvas);
       // 이벤트 리스너 추가
-      this.canvas.on('mouse:down', this.handleMouseDown)
-      this.canvas.on('mouse:move', this.handleMouseMove)
-      this.canvas.on('mouse:up', this.handleMouseUp)
+
+        this.canvas.on('mouse:down', (e) => {
+          if (this.mode == 'brush'){
+            const pointer = this.canvas.getPointer(e);
+            this.x = pointer.x
+            this.y = pointer.y
+            this.drawing = true;
+            this.handleMouseDown(e)
+          }
+        })
+      this.canvas.on('mouse:move', (e) => {
+        if (this.mode == 'brush' && this.drawing == true){
+          // prevXY 정의
+          this.prevX = this.x;
+          this.prevY = this.y;
+          this.handleMouseMove(e)
+        }
+
+      })
+      this.canvas.on('mouse:up', (e) => {
+        this.handleMouseUp(e)
+        this.canvas.off('mouse:off')
+      })
+
+      this.canvas.on('object:added', (e) => {
+        this.sendMessage(this.rect);
+      })
 
       console.log("initCanvas");
 
     },
     handleMouseDown(e) {
-      if (this.drawing) {
+      const pointer = this.canvas.getPointer(e);
+
+      this.brush = this.canvas.freeDrawingBrush;
+
+      let drawData = {
+        x: pointer.x,
+        y: pointer.y,
+        color: this.color,
+        width: this.lineWidth,
+      };
+
+      if (this.drawing == true) {
         this.canvas.isDrawingMode = true;
-        // this.canvas.freeDrawingBrush.color = this.color;
-        // this.canvas.freeDrawingBrush.width = this.lineWidth;
-        this.isMouseDown = true;
+
+        this.canvas.freeDrawingBrush.color = this.color;
+        this.canvas.freeDrawingBrush.width = parseInt(this.lineWidth);
+
+        console.log("마우스 다운")
       }
+      this.sendMessage('DRAW',drawData)
+
     },
     handleMouseMove(e) {
-      if (this.drawing && this.isMouseDown) {
-        // this.canvas.freeDrawingBrush.color = this.color;
-        // this.canvas.freeDrawingBrush.width = this.lineWidth;
 
-        console.log("마우스 무브")
-      }
+      const pointer = this.canvas.getPointer(e);
+      // 현재 좌표
+      this.x = pointer.x;
+      this.y = pointer.y;
 
+      let drawData = {
+        x: this.x,
+        y: this.y,
+        prevX: this.prevX,
+        prevY: this.prevY,
+        color: this.color,
+        width: this.lineWidth,
+      };
+
+      console.log("[this.prevXY] : ",this.prevX, this.prevY);
+      console.log("[this.xY] : ",this.x, this.y);
+      console.log("마우스 무브")
+      this.sendMessage('DRAW',drawData)
     },
     handleMouseUp(e) {
 
       this.drawing = false;
-      this.isMouseDown = true;
-      this.canvas.renderAll();
+
       console.log("마우스 업")
     },
 
@@ -200,25 +255,26 @@ export default {
         this.message = JSON.stringify({
           type: type,
           sender: this.sender,
-          data: {
-            draw: this.draw,
-            color: this.color,
-            lineWidth: this.lineWidth,
-          },
+          data: event,
         });
-        if (this.socket && this.socket.connected) {
-          this.socket.publish({
-            destination: `/pub/update/${this.classCode}`,
-            body: this.message,
-          });
-        }
+      } else if (type == 'rect') {
+        this.message = JSON.stringify({
+          type: type,
+          sender: this.sender,
+          data: event,
+        });
+      }
+      if (this.socket && this.socket.connected) {
+        this.socket.publish({
+          destination: `/pub/update/${this.classCode}`,
+          body: this.message,
+        });
       }
       console.log("메세지 전송 : ", this.message)
     },
 
     // 사각형 도형 추가 버튼
-    addRect(){
-
+    addRect(e){
       const rect = new fabric.Rect({
         // top: Math.random() * this.canvas.height,
         // left: Math.random() * this.canvas.width,
@@ -239,9 +295,9 @@ export default {
       this.rect = rect;
       this.canvas.add(rect);
       this.canvas.setActiveObject(rect);
-      this.canvas.renderAll();
-      this.sendMessage("rect",rect);
-      this.canvas.on('object:added')
+      // this.canvas.renderAll();
+
+      // this.sendMessage("rect",rect);
       console.log("rect", rect);
       console.log("도형 추가")
     },
@@ -267,7 +323,6 @@ export default {
       this.canvas.setActiveObject(circle);
       this.canvas.renderAll();
       this.sendMessage("circle",circle);
-      this.canvas.on('object:added')
       console.log("circle", circle);
       console.log("도형 추가")
     },
@@ -313,7 +368,7 @@ export default {
     // 그리기 버튼
     toggleDrawMode() {
       this.mode = 'brush';
-      this.drawing = true;
+
       this.canvas.isDrawingMode = true;
       this.canvas.freeDrawingBrush.color = this.color;
       this.canvas.freeDrawingBrush.lineWidth = this.lineWidth;
@@ -327,7 +382,7 @@ export default {
         this.drawing = false;
         this.mode = 'rect'
         console.log(this.mode)
-        this.addRect()
+        this.addRect(e)
 
       } else if(mode == 'circle') {
         this.canvas.isDrawingMode = false;
@@ -344,36 +399,56 @@ export default {
       }
     },
     clickObject(){
+      this.mode = '';
       this.canvas.isDrawingMode = false;
       this.drawing = false;
     },
 
     // 메시지 수신 처리 함수
     handleIncomingDrawing(message) {
-      const { type, data } = message;
-      if (type === "DRAW") {
-        console.log("draw 들어옴? : ",data);
-        // const { x, y, prevX, prevY, color, fillColor, lineWidth, rect, triangle, circle } = data;
-        // const { [x1, x2, y1, y2] color, lineWidth } = data;
-        const rcvDraw = new fabric.Line(data.draw,data);
-        this.canvas.add(rcvDraw);
-        console.log("받아서 그린 것 : ",rcvDraw);
-        this.canvas.renderAll();
+      const {type, data} = message;
+      console.log("받은 message : ", message);
 
-        // console.log(`보낸 메세지: , x: ${x} y: ${y} prevX: ${prevX}, prevY: ${prevY}, color: ${color}, fillColor: ${fillColor}, lineWidth:${lineWidth}, rect: ${rect}, triangle: ${triangle}, circle: ${circle}` , data);
+      if (type === 'DRAW') {
+        const { x, y, prevX, prevY, color, width } = data;
+
+        const context = this.canvas.getContext('2d');
+        context.strokeStyle = color;
+        context.lineWidth = width;
+        context.lineCap = this.lineCap;
+        context.beginPath();
+        context.moveTo(prevX, prevY);
+        context.lineTo(x, y);
+        context.stroke();
 
       }
-    },
+      if (type === 'rect'){
+        const newRect = new fabric.Rect(data);
+        this.canvas.add(newRect);
+        // this.canvas.renderAll();
+      }
+      if (type === 'circle'){
+        const newCircle = new fabric.Circle(data);
+        this.canvas.add(newCircle);
+      }
+      if (type === 'triangle'){
+        const triangle = new fabric.Triangle(data);
+        this.canvas.add(triangle);
+      }
+    }
   },
 };
 </script>
 
 <style scoped>
-
+.whiteboard {
+  width: 100%;
+  height: 100%;
+}
 .fabric {
   background: #2c4332;
-  /*width: 100%;
-  height: 100%;*/
+  width: 100%;
+  height: 100%;
 }
 
 #designTool {
