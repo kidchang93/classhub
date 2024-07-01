d07a0af7e9cc68e5fad15de96b9564b7fa626419
kidchang93 <lck930721@gmail.com>
2024-07-01 23:22:04 +0900

    feat : 각 속성들을 대입해도 소용이 없음

--- frontend/src/components/Whiteboard.vue
+++ frontend/src/components/Whiteboard.vue
@@ -689,7 +689,42 @@ export default {
 
       }
       if (type == 'rect' || type == 'modRect'){
-        const rect = new fabric.Rect(data);
+        console.log("받은 데이터 : ", data)
+        var rect = new fabric.Rect({
+          type:'rect',
+          originX	:	data.left,
+          originY	:	data.top,
+          left	:	data.left,
+          top	:	data.top,
+          width	:	data.width,
+          height	:	data.height,
+          fill: data.fill,
+          stroke	:	data.stroke,
+          strokeWidth	:	data.strokeWidth,
+          strokeDashArray	:	data.strokeDashArray,
+          strokeLineCap	:	data.strokeLineCap,
+          strokeDashOffset	:	data.strokeDashOffset,
+          strokeLineJoin	:	data.strokeLineJoin,
+          strokeUniform	:	data.strokeUniform,
+          strokeMiterLimit	:	data.strokeMiterLimit,
+          scaleX	:	data.scaleX,
+          scaleY	:	data.scaleY,
+          angle	:	data.angle,
+          flipX	:	data.flipX,
+          flipY	:	data.flipY,
+          opacity	:	data.opacity,
+          shadow	:	data.shadow,
+          visible	:	data.visible,
+          backgroundColor	: data.backgroundColor,
+          fillRule	:	data.fillRule,
+          paintFirst	:	data.paintFirst,
+          globalCompositeOperation	:	data.globalCompositeOperation,
+          skewX	:	data.skewX,
+          skewY	:	data.skewY,
+          rx	:	data.rx,
+          ry	:	data.ry,
+
+      });
 
         // let removeObjects = this.canvas.getObjects();
         // console.log("removeObject : ",removeObjects)
